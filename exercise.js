4ba7c2c04018fe33e1262fd7097df35f918bd643
Corri Golden <corri.golden@gmail.com>
2020-07-05 15:03:30 -0500

    practice

---
+++ exercise.js
@@ -0,0 +1,22 @@
+console.log("yo")
+
+// if(jQuery){
+//     alert("yep")
+// } else {
+//     alert("no")
+// }
+// $("div").css("background", "purple")
+
+// $(".highlight").css("width", "200px")
+
+// $("#third").css("border", " 2px solid orange")
+
+// $("div:first-of-type").css("color", "pink");
+
+$("h1").click(function(){
+    alert("stop")
+})
+
+$("button").click(function(){
+    alert("you crazy")
+})
